6a90ae1dd13bb809b2675e28e18c08f029879eac
a-sauvaget <alexandre.sauvaget44@gmail.com>
2021-12-04 23:29:17 +0100

    Finish doo guy hiter

--- assests/app.js
+++ assests/app.js
@@ -6,9 +6,6 @@ let app = {
     let spriteElement = document.getElementById('doomguy');
     spriteElement.addEventListener('click', app.handleClickOnSprite);
 
-    let resetButton = document.getElementById('resetButton');
-    resetButton.addEventListener('click', app.handleClickOnResetButton);
-
     let leftOver = document.getElementById('aside-left');
     leftOver.addEventListener('mouseover', app.handleLeftMouseover);
     leftOver.addEventListener('mouseleave', app.handleMouseleave);
@@ -18,41 +15,56 @@ let app = {
     rightOver.addEventListener('mouseleave', app.handleMouseleave);
   },
   handleClickOnSprite: function () {
-    let audio = new Audio("../assests/sound/DSPUNCH.wav");
+    let audio = new Audio('../assests/sound/DSPUNCH.wav');
     audio.play();
-    
-    app.counter += 1;
-    if (app.counter < 6) {
-      document.getElementById('doomguy').src = '../assests/sprite/DGF-' + app.counter + '.png';
-    }
 
+    if (app.counter < 5) {
+      app.counter += 1;
+      document.getElementById('doomguy').src =
+        '../assests/sprite/DGF-' + app.counter + '.png';
+    } else {
+      const footer = document.getElementById('footer');
+      console.log(footer);
+      const resetButton = document.createElement('button');
+      // '<button id="resetButton">Heal !</button>';
+      resetButton.setAttribute('id', 'resetButton');
+      resetButton.textContent = 'heal !';
+      resetButton.addEventListener('click', app.handleClickOnResetButton);
+      footer.appendChild(resetButton);
+      let spriteElement = document.getElementById('doomguy');
+      spriteElement.removeEventListener('click', app.handleClickOnSprite);
+    }
   },
   handleClickOnResetButton: function () {
-    let audio = new Audio("../assests/sound/DSBSPSIT.wav");
+    let audio = new Audio('../assests/sound/DSBSPSIT.wav');
     audio.play();
 
     app.counter = 1;
 
     document.getElementById('doomguy').src = '../assests/sprite/DGF-1.png';
+    const footer = document.getElementById('footer');
+    const resetButton = document.getElementById('resetButton');
+    footer.removeChild(resetButton);
   },
   handleLeftMouseover: function () {
     if (app.counter < 6) {
-    document.getElementById('doomguy').src = '../assests/sprite/DGL-' + app.counter + '.png';
+      document.getElementById('doomguy').src =
+        '../assests/sprite/DGL-' + app.counter + '.png';
     }
   },
   handleRightMouseover: function () {
     if (app.counter < 6) {
-    document.getElementById('doomguy').src = '../assests/sprite/DGR-' + app.counter + '.png';
+      document.getElementById('doomguy').src =
+        '../assests/sprite/DGR-' + app.counter + '.png';
     }
   },
   handleMouseleave: function () {
     if (app.counter < 6) {
-    document.getElementById('doomguy').src = '../assests/sprite/DGF-' + app.counter + '.png';
+      document.getElementById('doomguy').src =
+        '../assests/sprite/DGF-' + app.counter + '.png';
     }
   },
 };
 
 // Quand la page est entièrement chargée, on exécute la méthode init située dans l'object app.
 document.addEventListener('DOMContentLoaded', app.init);
-
-
